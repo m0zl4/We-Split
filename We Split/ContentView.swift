4fbb9f7db5323fae50452040146b124625ce9951
m0zl4 <zayuka040104@gmail.com>
2021-04-05 10:04:15 +0700

    Initial Commit

---
+++ We Split/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  We Split
+//
+//  Created by M. Abdul Latief -Mz on 05/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
